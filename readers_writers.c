13322c5d2ffc6527ba1983bb49b99877f61f7983
David Kendall <david.kendall@northumbria.ac.uk>
2017-11-16 13:28:34 +0000

    RW solution avoiding writer starvation

--- readers_writers.c
+++ readers_writers.c
@@ -1,6 +1,8 @@
 /**
- * A monitor solution to the readers/writers problem.
+ * A monitor solution to the readers/writers problem
+ * avoiding writer starvation
  */
+#include <stdbool.h>
 #include <pthread.h>
 #include <stdio.h>
 #include "readers_writers.h"
@@ -12,6 +14,7 @@ static unsigned int ar = 0; /** number of active readers */
 static unsigned int wr = 0; /** number of waiting readers */
 static unsigned int aw = 0; /** number of active writers */
 static unsigned int ww = 0; /** number of waiting writers */
+static bool writerPriority = false; /** used to avoid writer starvation */
 
 void rw_init(void) {
   pthread_mutex_init(&monitorMutex, NULL);
@@ -33,6 +36,7 @@ void rw_write(rw_func_t do_write, void *arg) {
 
   pthread_mutex_lock(&monitorMutex);
   aw -= 1;
+  writerPriority = false;
   pthread_cond_broadcast(&okToContinue);
   pthread_mutex_unlock(&monitorMutex);
 }
@@ -40,7 +44,7 @@ void rw_write(rw_func_t do_write, void *arg) {
 
 void rw_read(rw_func_t do_read, void *arg) {
   pthread_mutex_lock(&monitorMutex);
-  while (aw > 0) {
+  while ((aw > 0) || ((ww > 0) && writerPriority)) {
     wr += 1;
     pthread_cond_wait(&okToContinue, &monitorMutex);
     wr -= 1;
@@ -53,6 +57,7 @@ void rw_read(rw_func_t do_read, void *arg) {
 
   pthread_mutex_lock(&monitorMutex);
   ar -= 1;
+  writerPriority = true;
   if (ar == 0) {
     pthread_cond_broadcast(&okToContinue);
   }
