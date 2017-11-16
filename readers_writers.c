763c7582976e44817189262a5201d2dfda9cae1f
David Kendall <david.kendall@northumbria.ac.uk>
2017-11-16 13:16:54 +0000

    RW solution with possible writer starvation

--- readers_writers.c
+++ readers_writers.c
@@ -1,15 +1,12 @@
 /**
  * A monitor solution to the readers/writers problem.
- * Note that this solution may suffer from *reader* starvation
- * rather than *writer* starvation
  */
 #include <pthread.h>
 #include <stdio.h>
 #include "readers_writers.h"
 
 static pthread_mutex_t monitorMutex;
-static pthread_cond_t okToRead;
-static pthread_cond_t okToWrite;
+static pthread_cond_t okToContinue;
 
 static unsigned int ar = 0; /** number of active readers */
 static unsigned int wr = 0; /** number of waiting readers */
@@ -18,15 +15,14 @@ static unsigned int ww = 0; /** number of waiting writers */
 
 void rw_init(void) {
   pthread_mutex_init(&monitorMutex, NULL);
-  pthread_cond_init(&okToRead, NULL);
-  pthread_cond_init(&okToWrite, NULL);
+  pthread_cond_init(&okToContinue, NULL);
 }
 
 void rw_write(rw_func_t do_write, void *arg) {
   pthread_mutex_lock(&monitorMutex);
   while (ar + aw > 0) {
     ww += 1;
-    pthread_cond_wait(&okToWrite, &monitorMutex);
+    pthread_cond_wait(&okToContinue, &monitorMutex);
     ww -= 1;
   }
   aw += 1;
@@ -37,20 +33,16 @@ void rw_write(rw_func_t do_write, void *arg) {
 
   pthread_mutex_lock(&monitorMutex);
   aw -= 1;
-  if (ww > 0) {
-    pthread_cond_signal(&okToWrite);
-  } else if (wr > 0) {
-    pthread_cond_broadcast(&okToRead);
-  }
+  pthread_cond_broadcast(&okToContinue);
   pthread_mutex_unlock(&monitorMutex);
 }
 
 
 void rw_read(rw_func_t do_read, void *arg) {
   pthread_mutex_lock(&monitorMutex);
-  while (aw + ww > 0) {
+  while (aw > 0) {
     wr += 1;
-    pthread_cond_wait(&okToRead, &monitorMutex);
+    pthread_cond_wait(&okToContinue, &monitorMutex);
     wr -= 1;
   }
   ar += 1;
@@ -61,8 +53,8 @@ void rw_read(rw_func_t do_read, void *arg) {
 
   pthread_mutex_lock(&monitorMutex);
   ar -= 1;
-  if ((ar == 0) && (ww > 0)) {
-    pthread_cond_signal(&okToWrite);
+  if (ar == 0) {
+    pthread_cond_broadcast(&okToContinue);
   }
   pthread_mutex_unlock(&monitorMutex);
 }
